eb3ff6312a26dd8a2b4ff7cddee7844adc3720cc
づ｡◕‿◕｡)づ lucylow <low.lucyy@gmail.com>
2026-09-09 15:25:33 -0400

    .

--- scene/src/hybrid/index.ts
+++ scene/src/hybrid/index.ts
@@ -2,6 +2,7 @@ import { createHybridWorldDataset } from './generator'
 import { simulationFromDataset } from './protocol'
 import { assembleHybridWorld } from './renderer'
 import { startHybridAmbientLoop } from './ambient'
+import { SceneErrorHandler } from '../systems/errorHandling'
 
 export { createHybridWorldDataset } from './generator'
 export { assembleHybridWorld } from './renderer'
@@ -10,11 +11,15 @@ export { tickHybridSimulation } from './simulation'
 export { HYBRID_WORLD_SEED } from './constants'
 
 export function bootstrapHybridWorld(): void {
-  const dataset = createHybridWorldDataset()
-  assembleHybridWorld(dataset)
-  startHybridAmbientLoop({
-    elapsed: 0,
-    dataset,
-    state: simulationFromDataset(dataset),
-  })
+  try {
+    const dataset = createHybridWorldDataset()
+    assembleHybridWorld(dataset)
+    startHybridAmbientLoop({
+      elapsed: 0,
+      dataset,
+      state: simulationFromDataset(dataset),
+    })
+  } catch (error) {
+    SceneErrorHandler.getInstance().recordFault('Hybrid demo universe unavailable', error)
+  }
 }
